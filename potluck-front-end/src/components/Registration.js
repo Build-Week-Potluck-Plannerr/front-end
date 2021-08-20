9e13fdecb5cb4afb17d611775abc8f181b2af3e8
Craig Barcinas <cbarcinas@live.com>
2021-08-19 19:43:32 -0700

    created registration form component

--- potluck-front-end/src/components/Registration.js
+++ potluck-front-end/src/components/Registration.js
@@ -1,1 +1,37 @@
-import React from 'react'
+import React, { useState } from "react";
+
+const initialValues = {
+  username: "",
+  name: "",
+  password: "",
+};
+
+const Registration = () => {
+  const [user, setUser] = useState(initialValues);
+
+  return (
+    <form>
+      <h2>Create An Account</h2>
+      <input
+        type="text"
+        name="username"
+        placeholder="Username"
+        onChange={onInputChange}
+      />
+      <input
+        type="text"
+        name="name"
+        placeholder="Name"
+        onChange={onInputChange}
+      />
+      <input
+        type="password"
+        name="password"
+        placeholder="Password"
+        onChange={onInputChange}
+      />
+    </form>
+  );
+};
+
+export default Registration;
