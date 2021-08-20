1ba66d0a99ca9971699e86e2dc33d41c431794be
mikelovelace <mike@mlovelace.net>
2021-08-19 22:46:58 -0400

    built basic login skeleton

--- potluck-front-end/src/utils/axiosWithAuth.js
+++ potluck-front-end/src/utils/axiosWithAuth.js
@@ -3,7 +3,6 @@ import axios from "axios";
 export const axiosWithAuth = () => {
   const token = localStorage.getItem("token");
 
-  //returning axios returns promise rather than resolving it
   return axios.create({
     baseURL: "http://localhost:5000/api",
     headers: {
