d2ed83009bd022b0caeea2a161e8349f2425f574
mikelovelace <mike@mlovelace.net>
2021-08-19 23:37:38 -0400

    added some routes

--- potluck-front-end/src/App.js
+++ potluck-front-end/src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import Login from './components/Login';
+import Registration from './components/Registration';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <div>
+        <h1>Potluck Planner</h1>
+
+        <Route path='/login'>
+          <Login />
+        </Route>
+
+        <Route path='/register'>
+          <Registration />
+        </Route>
+
+      </div>
+    </Router>
   );
 }
 
