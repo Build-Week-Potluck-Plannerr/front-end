8bfea80f42c742350f34e329156e775d5d193cb0
Craig Barcinas <cbarcinas@live.com>
2021-08-20 20:14:49 -0700

    added redux store and middlware to redux store in index.js

--- potluck-front-end/src/index.js
+++ potluck-front-end/src/index.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+
+import rootReducer from "./api/rootReducer";
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById("root")
 );
-
