53cda216041dd64c606d97aac6da3dffa542694c
mikelovelace <mike@mlovelace.net>
2021-08-25 20:54:12 -0400

    added toggle menu but more to be done

--- potluck-front-end/src/App.js
+++ potluck-front-end/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import Nav from './components/Nav';
 import Login from "./components/Login";
 import Registration from "./components/Registration";
 
@@ -7,15 +8,17 @@ import PrivateRoute from "./utils/PrivateRoute";
 import Potluck from "./components/Potluck";
 
 function App() {
+
   return (
     <Router>
       <div>
+        <Nav />
         <h1>Potluck Planner</h1>
         <Route exact path="/" component={Login} />
         <PrivateRoute path="/potluck" component={Potluck} />
+        <Route path="/register" component={Registration} />
+        <Route path="/login" component={Login} />
       </div>
-      {/* <Route path="/login" component={Login} /> */}
-      <Route path="/register" component={Registration} />
     </Router>
   );
 }
---
+++ potluck-front-end/src/components/Nav.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import '../styles/nav.css'
+
+function Nav() {
+  const [navToggle, setNavToggle] = useState(false);
+
+  const handleLogout = () => {
+    localStorage.clear();
+    window.location.replace('/');
+  };
+
+  return (
+    <div className="nav-header">
+
+      {
+        navToggle &&
+        <div className="side-menu-container">
+          <div><Link to="/">Home</Link></div>
+          <div><Link to="/">Host Potluck</Link></div>
+          <div><Link to="/">Create Potluck</Link></div>
+          <div><Link to="/">My Potlucks</Link></div>
+          <div><Link to="/">Logout</Link></div>
+        </div>
+      }
+
+      <div className="nav-links">
+        <a className="menu-open" href="#" onClick={() => setNavToggle(!navToggle)}>Menu</a>
+      </div>
+
+      <div className="hello-user">
+        <p>$Component Name</p>
+      </div>
+
+      <div className="logout-container">
+        <Link to="/logout" onClick={handleLogout}>
+          Logout
+        </Link>
+      </div>
+
+    </div>
+  )
+}
+
+export default Nav
