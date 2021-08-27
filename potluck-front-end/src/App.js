17ad27236b2c1ab61cba4c0be0dea65f92cd456d
mikelovelace <mike@mlovelace.net>
2021-08-27 15:29:19 -0400

    added about placeholder

--- potluck-front-end/src/App.js
+++ potluck-front-end/src/App.js
@@ -3,15 +3,17 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Registration from "./components/Registration";
-
-import PrivateRoute from "./utils/PrivateRoute";
 import Potluck from "./components/Potluck";
+import About from "./components/About";
+import PrivateRoute from "./utils/PrivateRoute";
+
 
 function App() {
   return (
     <Router>
       <div>
         <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
         {/* <PrivateRoute path="/potluck" component={Potluck} /> */}
         <Route path="/potluck" component={Potluck} />
         <Route path="/register" component={Registration} />
---
+++ potluck-front-end/src/components/About.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Nav from "./Nav";
+import "../styles/about.css"
+
+function About() {
+  return (
+    <div>
+      <Nav />
+      <div className="about-container">
+
+        <div className="about">
+          <h2>ABOUT US</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tristique sollicitudin nibh sit amet commodo nulla facilisi nullam. Nec ultrices dui sapien eget mi proin sed. Imperdiet massa tincidunt nunc pulvinar sapien et ligula ullamcorper. Non enim praesent elementum facilisis leo vel fringilla. Est ultricies integer quis auctor elit. Faucibus vitae aliquet nec ullamcorper sit. Urna id volutpat lacus laoreet non curabitur gravida arcu ac. Morbi tincidunt augue interdum velit euismod. Sed pulvinar proin gravida hendrerit lectus a. Bibendum enim facilisis gravida neque convallis a cras. Scelerisque eu ultrices vitae auctor eu augue. Viverra adipiscing at in tellus integer feugiat scelerisque varius.</p>
+
+          <p>Purus in mollis nunc sed id. Varius duis at consectetur lorem. Dapibus ultrices in iaculis nunc sed augue lacus viverra vitae. Tincidunt lobortis feugiat vivamus at augue eget arcu dictum. Id neque aliquam vestibulum morbi blandit cursus risus at. Faucibus in ornare quam viverra orci sagittis eu. Tortor dignissim convallis aenean et tortor at risus viverra. Commodo viverra maecenas accumsan lacus vel facilisis volutpat est. Elementum sagittis vitae et leo duis. Nulla facilisi etiam dignissim diam quis enim. Nibh mauris cursus mattis molestie a iaculis. Egestas diam in arcu cursus euismod quis viverra. Vulputate odio ut enim blandit volutpat maecenas volutpat blandit aliquam. Morbi tempus iaculis urna id volutpat. Vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant morbi tristique. Amet venenatis urna cursus eget. Egestas diam in arcu cursus euismod quis.</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default About
